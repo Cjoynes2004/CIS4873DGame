4566aeaff3f4c1dd47060797074434b9198d55ce
DevaunThomas <devaunthomas@gmail.com>
2025-12-02 15:13:34 -0500

    Main Menu,Controls
    Added functions to all buttons and the controls screen

--- MurderOnTheRocks/Assets/Scripts/Menu Button Scripts/MainMenuButtons.cs
+++ MurderOnTheRocks/Assets/Scripts/Menu Button Scripts/MainMenuButtons.cs
@@ -5,8 +5,25 @@ public class MainMenuButtons : MonoBehaviour
 {   
     public void PlayGame()
     {
-        SceneManager.LoadScene(1); // loads scene at index 1
+        SceneManager.LoadScene(2); // loads scene at index 2(level 1)
     }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is closing...");
+    }
+
+    public void ControlMenu()
+    {
+        SceneManager.LoadScene(1); // loads scene at index 1(Controls Screen)
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);  // loads scene at index 0(Main Menu)
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
