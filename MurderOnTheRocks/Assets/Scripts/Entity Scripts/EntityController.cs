b205288f42f07a521ab33c44db476d261ef857a8
Elfykins123 <efoucher789@gmail.com>
2025-12-16 19:51:53 -0500

    Lights Flickering when Entity Enters
    Lights will now flicker when the entity enters the bar

--- MurderOnTheRocks/Assets/Scripts/Entity Scripts/EntityController.cs
+++ MurderOnTheRocks/Assets/Scripts/Entity Scripts/EntityController.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 using UnityEngine.Playables;
+using System.Collections;
 
 public class EntityController : MonoBehaviour
 {
+
+    public Light[] barLights;
+
     public void OnPlayerClicked()
     {
         Debug.Log("Entity clicked - giving order!");
@@ -14,4 +18,32 @@ public class EntityController : MonoBehaviour
         // Plug in UI here
         Debug.Log("The Entity whispers: '...Bring me something... foul...'");
     }
+
+    public void StartEntranceEffects()
+    {
+        StartCoroutine(FlickerLights());
+    }
+
+    IEnumerator FlickerLights()
+    {
+        foreach (Light light in barLights)
+        {
+            StartCoroutine(FlickerSingleLight(light));
+            yield return new WaitForSeconds(Random.Range(0.2f, 0.6f));
+        }
+    }
+
+    IEnumerator FlickerSingleLight(Light light)
+    {
+        int flickers = Random.Range(3, 6);
+
+        for (int i = 0; i < flickers; i++)
+        {
+            light.enabled = false;
+            yield return new WaitForSeconds(Random.Range(0.05f, 0.15f));
+            light.enabled = true;
+            yield return new WaitForSeconds(Random.Range(0.05f, 0.2f));
+        }
+    }
+
 }
